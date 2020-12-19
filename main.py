bcff90bc8708d56c8b2ca55c1c3d17a8522d4d8f
Саня Череп <41489405+SDesya74@users.noreply.github.com>
2020-12-19 12:48:28 +0300

    Change design & add weather

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
+from requests import get
+
 from PyQt5 import uic, QtWidgets
 from PyQt5.QtWidgets import QApplication
 
 Form, Window = uic.loadUiType("weather-ui.ui")
+token = "801db4d874c0fd13fadef4c681a48929"
 
 
 class App(QtWidgets.QMainWindow, Form):
@@ -11,7 +14,14 @@ class App(QtWidgets.QMainWindow, Form):
         self.pushButton.clicked.connect(self.on_click)
 
     def on_click(self):
-        print("click")
+        city_name = self.cityField.toPlainText()
+        json = get("https://api.openweathermap.org/data/2.5/weather", {
+            "q": city_name,
+            "appid": token,
+            "units": "metric",
+            "lang": "ru"
+        }).json()
+        self.temperatureLabel.setText(str(json["main"]["temp"]))
 
 
 app = QApplication([])
