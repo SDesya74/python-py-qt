346427295a93edec71409ceee72ad68b5e56f949
Саня Череп <41489405+SDesya74@users.noreply.github.com>
2020-12-19 13:46:35 +0300

    Add dialog

--- main.py
+++ main.py
@@ -4,7 +4,15 @@ from PyQt5 import uic, QtWidgets
 from PyQt5.QtWidgets import QApplication
 
 Form, Window = uic.loadUiType("weather-ui.ui")
+Form2, Dialog = uic.loadUiType("dialog.ui")
 token = "801db4d874c0fd13fadef4c681a48929"
+
+
+class DialogWindow(QtWidgets.QDialog, Form2):
+    def __init__(self):
+        super(DialogWindow, self).__init__()
+        self.setupUi(self)
+        # self.pushButton.clicked.connect(self.on_click)
 
 
 class App(QtWidgets.QMainWindow, Form):
@@ -12,9 +20,13 @@ class App(QtWidgets.QMainWindow, Form):
         super(App, self).__init__()
         self.setupUi(self)
         self.pushButton.clicked.connect(self.on_click)
+        self.findButton.clicked.connect(self.on_find_click)
+
+    def on_find_click(self):
+        dialog.show()
 
     def on_click(self):
-        city_name = self.cityField.toPlainText()
+        city_name = self.cityField.text()
         json = get("https://api.openweathermap.org/data/2.5/weather", {
             "q": city_name,
             "appid": token,
@@ -26,5 +38,6 @@ class App(QtWidgets.QMainWindow, Form):
 
 app = QApplication([])
 window = App()
+dialog = DialogWindow()
 window.show()
 app.exec_()
